6d54ab1c83ddb90670b0f8d54f46fe78392e506c
KenadiFredrichs <814769@mystma.org>
2020-09-14 20:17:13 +0000

    Movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Git rect haha");
+        System.out.println("A Quiet Place");
+        System.out.println("Hush");
+        System.out.println("Death Note");
+        System.out.println("Coraline");
+        System.out.println("A Silent Voice");
+        System.out.println("Two Heros");
+        System.out.println("Heros Rising");
+        System.out.println("Alice In Wonderland");
+        System.out.println("The Lorax");
+        System.out.println("Mulan");
+    }
+}
