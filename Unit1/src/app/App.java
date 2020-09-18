ff0d0ed15ed6629667aabd1da4582c4a1590d671
KenadiFredrichs <814769@mystma.org>
2020-09-18 18:16:56 +0000

    8 codes

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,16 +2,34 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Git rect haha");
-        System.out.println("A Quiet Place");
-        System.out.println("Hush");
-        System.out.println("Death Note");
-        System.out.println("Coraline");
-        System.out.println("A Silent Voice");
-        System.out.println("Two Heros");
-        System.out.println("Heros Rising");
-        System.out.println("Alice In Wonderland");
-        System.out.println("The Lorax");
-        System.out.println("Mulan");
+        Boolean likeCats = true;
+        Byte letInAlph = 26;
+        char firLetDog = 'K';
+        short sidOfOct = 8;
+        int hairOnHe = 100000;
+        long visStInSky = 9096;
+        float gInCOfSou = (float) 254.66;
+        double oneForInDec = 0.25;
+        
+
+        System.out.println("What is one fourth in decimals?");
+        System.out.println(oneForInDec);
+        System.out.println("How many grams are in a cup of soup?");
+        System.out.println(gInCOfSou);
+        System.out.println("How many visable stars are in the sky?");
+        System.out.println(visStInSky);
+        System.out.println("How many hairs on a human head?");
+        System.out.println(hairOnHe);
+        System.out.println("How many sides does an octogon have?");
+        System.out.println(sidOfOct);
+        System.out.println("Whats the first letter of my dogs name?");
+        System.out.println(firLetDog);
+        System.out.println("Do I like cats?");
+        System.out.println(likeCats);
+        System.out.println("How many letters in the alphabet?");
+        System.out.println(letInAlph);
+
+
+
     }
 }
