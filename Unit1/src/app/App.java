ce3fe56a7a990ee30cd44dd5067d57c89d4f96f2
KenadiFredrichs <814769@mystma.org>
2020-09-18 19:11:37 +0000

    Mad Lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,34 +2,27 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Boolean likeCats = true;
-        Byte letInAlph = 26;
-        char firLetDog = 'K';
-        short sidOfOct = 8;
-        int hairOnHe = 100000;
-        long visStInSky = 9096;
-        float gInCOfSou = (float) 254.66;
-        double oneForInDec = 0.25;
-        
+        String person1 = "Nolan";
+        String person2 = "Erik";
+        String verb1 = "run";
+        String verb2 = "lick";
+        String verb3 = "feel";
+        String food = "watermelon";
+        String dayOfWeek = "Friday";
+        String place1 = "Mcdonalds";
+        String place2 = "A park";
+        String adjective1 = "jolly";
+        String adjective2 = "large";
 
-        System.out.println("What is one fourth in decimals?");
-        System.out.println(oneForInDec);
-        System.out.println("How many grams are in a cup of soup?");
-        System.out.println(gInCOfSou);
-        System.out.println("How many visable stars are in the sky?");
-        System.out.println(visStInSky);
-        System.out.println("How many hairs on a human head?");
-        System.out.println(hairOnHe);
-        System.out.println("How many sides does an octogon have?");
-        System.out.println(sidOfOct);
-        System.out.println("Whats the first letter of my dogs name?");
-        System.out.println(firLetDog);
-        System.out.println("Do I like cats?");
-        System.out.println(likeCats);
-        System.out.println("How many letters in the alphabet?");
-        System.out.println(letInAlph);
-
-
-
+        System.out.println(person1 + "Is a hard worker that loves to " + verb1);
+        System.out.println(" Every " + dayOfWeek);
+        System.out.println(" him and " + person2);
+        System.out.println(" go to " + place2);
+        System.out.println(" to make some " + food);
+        System.out.println(" at the park they always see a " + adjective1);
+        System.out.println(" old man and offer him a " + verb2);
+        System.out.println(" when they leave they have a " + verb3);
+        System.out.println(" on where to find the " + adjective2);
+        System.out.println(place1 + " play place to eat ");
     }
 }
