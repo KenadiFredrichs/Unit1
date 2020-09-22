77a889463eb93d3f4b72bd92892e56f721bb791b
KenadiFredrichs <814769@mystma.org>
2020-09-22 02:29:31 +0000

    madlib2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,28 +1,21 @@
 package app;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Nolan";
-        String person2 = "Erik";
-        String verb1 = "run";
-        String verb2 = "lick";
-        String verb3 = "feel";
-        String food = "watermelon";
-        String dayOfWeek = "Friday";
-        String place1 = "Mcdonalds";
-        String place2 = "A park";
-        String adjective1 = "jolly";
-        String adjective2 = "large";
+        Scanner input = new Scanner(System.in);
+        System.out.println(input);
 
-        System.out.println(person1 + "Is a hard worker that loves to " + verb1);
-        System.out.println(" Every " + dayOfWeek);
-        System.out.println(" him and " + person2);
-        System.out.println(" go to " + place2);
-        System.out.println(" to make some " + food);
-        System.out.println(" at the park they always see a " + adjective1);
-        System.out.println(" old man and offer him a " + verb2);
-        System.out.println(" when they leave they have a " + verb3);
-        System.out.println(" on where to find the " + adjective2);
-        System.out.println(place1 + " play place to eat ");
+        System.out.println("Write a verb.");
+        String verb = input.nextLine();
+        System.out.println("Write a place.");
+        String place = input.nextLine();
+        System.out.println("Write a verb.");
+        String verb2 = input.nextLine();
+        System.out.println("Write a name.");
+        String name = input.nextLine();
+        System.out.println("Write a food.");
+        String food = input.nextLine();
+
+        System.out.println("It started off at the " + place + ". As always you went with " + name + ", who is a very good friend of yours. You " + verb + " all over the place. You asked " + name + " if they wanted to get food. They said yes and then you both " + verb2 + " to your favorite food truck that had " + food);
     }
 }
