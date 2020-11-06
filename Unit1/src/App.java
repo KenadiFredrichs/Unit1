bde35eff5c15e6a5d2c811739d7bf54c7a8a888a
KenadiFredrichs <814769@mystma.org>
2020-11-06 20:46:06 +0000

    Challange 2

--- Unit1/src/App.java
+++ Unit1/src/App.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(final String[] args) throws Exception {
 
-        for (int i = 0; i <= 1000; i++){
-            System.out.println(i);
+        Scanner input = new Scanner(System.in);
+        String password = "Banana Phone";
+        int tries = 0;
+        boolean secretMess = false;
+
+        while (tries < 3) {
+            if (secretMess == false) {
+                System.out.println("Enter the password");
+                String enterPass = input.nextLine();
+
+                if (enterPass.equals(password)) {
+                    System.out.println("Accepted");
+                    System.out.println("Your fridge is running");
+                    secretMess = true;
+                }
+
+                else {
+                    System.out.println("Declined");
+                    tries++;
+                    System.out.println("You have " + (3 - tries) + "remaining");
+                }
+            }
+        }
+
+        if (tries == 3) {
+            System.out.println("Recieved limited amount of tries");
         }
     }
     }
