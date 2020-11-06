303b99ba8c57404ef8625e97618d251652a82643
KenadiFredrichs <814769@mystma.org>
2020-11-06 20:54:36 +0000

    Challange 3

--- Unit1/src/App.java
+++ Unit1/src/App.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(final String[] args) throws Exception {
 
-        for (int i = 0; i <=1000; i++) {
-            System.out.println(i);
+        Scanner input = new Scanner(System.in);
+        int num = 34;
+        boolean finish = false;
+
+        while (finish == false) {
+            System.out.println("Pick a number between 0 and 100");
+            int guess = input.nextInt();
+            input.nextLine();
+
+            if (guess == num) {
+                System.out.println("Correct");
+                finish = true;
+            }
+
+            else if (guess < num) {
+                System.out.println("Too low");
+            }
+
+            else {
+                System.out.println("Too high");
+            }
         }
+
     }
     }
 
